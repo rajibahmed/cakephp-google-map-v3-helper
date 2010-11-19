7bfdf15c5a2a119ef381f788e08c1406250cb088
Rajib Ahmed <l.rajibahmed@gmail.com>
2010-11-19 02:03:14 +0100

    Markers added

--- google_map_v3.php
+++ google_map_v3.php
@@ -1,178 +1,84 @@
-<?php
-
-/*
-      CakePHP Google Map V3 - Helper to CakePHP framework that integrates a Google Map in your view
-      using Google Maps API V3.
-
-    Copyright (c) 2010 Marc Fernandez Girones: info@marcfg.com
-
-    MIT LICENSE:
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-
-    MarcFG : http://www.marcfg.com
-
-    @author      Marc Fernandez Girones <info@marcfg.com>
-    @version     1.0
-    @license     OPPL
-
-    Date         May 13, 2010
-
-    USAGE:
-
-    In your CONTROLLER:
-        var $helpers = array('GoogleMapV3');    Add the helper
-
-      In your VIEW:
-          To add a map that localizes you:
-              echo $googleMapV3->map();
-
-          OR
-
-          You can also pass to the function a variable with any of the followings options and change the default parameters
-              $mapOptions= array(
-                'width'=>'800px',                //Width of the map
-                'height'=>'800px',                //Height of the map
-                'zoom'=>7,                        //Zoom
-                'type'=>'HYBRID',                 //Type of map (ROADMAP, SATELLITE, HYBRID or TERRAIN)
-                'latitude'=>40.69847032728747,    //Default latitude if the browser doesn't support localization or you don't want localization
-                'longitude'=>-1.9514422416687,    //Default longitude if the browser doesn't support localization or you don't want localization
-                'localize'=>true,                //Boolean to localize your position or not
-                'marker'=>true,                    //Boolean to put a marker in the position or not
-                'markerIcon'=>'http://google-maps-icons.googlecode.com/files/home.png',    //Default icon of the marker
-                'infoWindow'=>true,                //Boolean to show an information window when you click the marker or not
-                'windowText'=>'My Position'        //Default text inside the information window
-            );
-            echo $googleMapV3->map($mapOptions); To add a map that localizes you
-
-        To add a marker:
-              echo $googleMapV3->addMarker(array('latitude'=>40.69847,'longitude'=>-73.9514));
-
-          OR
-
-          You can also pass to the function a variable with any of the followings options and change the default parameters
-        $markerOptions= array(
-            'id'=>1                                //Id of the marker
-            'latitude'=>40.69847032728747,        //Latitude of the marker
-            'longitude'=>-1.9514422416687,        //Longitude of the marker
-            'markerIcon'=>'http://google-maps-icons.googlecode.com/files/home.png', //Custom icon
-            'shadowIcon'=>'http://google-maps-icons.googlecode.com/files/home.png', //Custom shadow
-            'infoWindow'=>true,                    //Boolean to show an information window when you click the marker or not
-            'windowText'=>'Marker'                //Default text inside the information window
-        );
-
-      This helper uses the latest Google API V3 so you don't need to provide or get any Google API Key
-*/
+<?php 
 
 class GoogleMapV3Helper extends Helper {
-
-    var $helpers=array('Javascript', 'Html');
-
+ 
+    var $helpers=array('Javascript', 'Html'); 
+    
 
     private $_defaultSettings = array(
       'zoom'    =>6,
-      'type'    =>'HYBRID',
+      'type'    =>'ROADMAP',
       'longitude'=>-73.95144,
       'latitude'=>40.698,
       'localize'=>true,
       'showMarker'  =>true,
       'showInfoWindow'=>true,
-      'icon'=>'http://google-maps-icons.googlecode.com/files/home.png',
+      'markerIcon'=>'http://google-maps-icons.googlecode.com/files/home.png',
+      'infoWindowText'=>'Change Me',
       'div'=>array(
         'id'=>'map_canvas',
-        'style'=>'width:600px; height:600px'
+        'style'=>'width:500px; height:500px'
        ),
       'usesJquery'=>true
-    );
-   //DEFAULT MAP OPTIONS (function map())
-    var $defaultWidth="800px";                    //Width of the map
-    var $defaultHeight="800px";                    //Height of the map
-    var $defaultZoom=6;                            //Default zoom
-    var $defaultType='HYBRID';                    //Type of map (ROADMAP, SATELLITE, HYBRID or TERRAIN)
-    var $defaultLatitude=40.69847032728747;        //Default latitude if the browser doesn't support localization or you don't want localization
-    var $defaultLongitude=-73.9514422416687;    //Default longitude if the browser doesn't support localization or you don't want localization
-    var $defaultLocalize=true;                    //Boolean to localize your position or not
-    var $defaultMarker=true;                    //Boolean to put a marker in the position or not
-    var $defaultMarkerIcon='http://google-maps-icons.googlecode.com/files/home.png'; //Default icon of the marker
-    var $defaultInfoWindow=true;                //Boolean to show an information window when you click the marker or not
-    var $defaultWindowText='My Position';        //Default text inside the information window
-
-    //DEFAULT MARKER OPTIONS (function addMarker())
-    var $defaultInfoWindowM=true;        //Boolean to show an information window when you click the marker or not
-    var $defaultWindowTextM=' ';        //Default text inside the information window
-
-
-   /**
-     * Function map
-     *
+      );
+        
+   /** 
+     * Function map 
+     * 
      * This method generates a tag called map_canvas and insert
      * a google maps.
-     *
+     * 
      * Pass an array with the options listed above in order to customize it
-     *
-     * @author Marc Fernandez <info (at) marcfg (dot) com>
-     * @param array $options - options array
+     * 
+     * @author Marc Fernandez <info (at) marcfg (dot) com> 
+     * @param array $options - options array 
      * @return string - will return all the javascript script to generate the map
-     *
-     */
+     * 
+     */    
     function map($options=null){
-      $settings = Set::merge($this->_defaultSettings,$options);
-
+      $settings = Set::merge($this->_defaultSettings,$options);    
+      
       $this->Javascript->link('http://maps.google.com/maps/api/js?sensor=true',false);
       $this->Javascript->link("http://code.google.com/apis/gears/gears_init.js",false);
-
+    
       $map = $this->Html->div('','',$settings['div']);
         $map .= "
         <script type='text/javascript'>
             var noLocation = new google.maps.LatLng(".$settings['latitude'].", ".$settings['longitude'].");
             var initialLocation;
             var browserSupportFlag =  new Boolean();
-            var map;
             var myOptions = {
               zoom: ".$settings['zoom'].",
               mapTypeId: google.maps.MapTypeId.".$settings['type'].",
               center:noLocation
             };
-            var map = new google.maps.Map(document.getElementById(\"".$settings['div']['id']."\"), myOptions);
+              
+            //Global variables	  
+            gMap = new google.maps.Map(document.getElementById(\"".$settings['div']['id']."\"), myOptions);
+            gMarkers = new Array();
         ";
-
-        //if($localize) $map .= "localize();"; else $map .= "map.setCenter(noLocation);";
+        
         /*$map .= "
             function localize(){
                 if(navigator.geolocation) { // Try W3C Geolocation method (Preferred)
                     browserSupportFlag = true;
                     navigator.geolocation.getCurrentPosition(function(position) {
                       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-                      map.setCenter(initialLocation);";
-                      if($marker) $map .= "setMarker(initialLocation);";
-
+                      gMap.setCenter(initialLocation);";
+                      if($settings['showMarker']) $map .= "setMarker(initialLocation);";
+                               
                     $map .= "}, function() {
                       handleNoGeolocation(browserSupportFlag);
                     });
-
+                    
                 } else if (google.gears) { // Try Google Gears Geolocation
                     browserSupportFlag = true;
                     var geo = google.gears.factory.create('beta.geolocation');
                     geo.getCurrentPosition(function(position) {
                       initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
-                      map.setCenter(initialLocation);";
-                      if($marker) $map .= "setMarker(initialLocation);";
-
+                      gMap.setCenter(initialLocation);";
+                      if($settings['showMarker']) $map .= "setMarker(initialLocation);";         
+                
                     $map .= "}, function() {
                       handleNoGeolocation(browserSupportFlag);
                     });
@@ -182,7 +88,7 @@ class GoogleMapV3Helper extends Helper {
                     handleNoGeolocation(browserSupportFlag);
                 }
             }
-
+            
             function handleNoGeolocation(errorFlag) {
                 if (errorFlag == true) {
                   initialLocation = noLocation;
@@ -191,32 +97,32 @@ class GoogleMapV3Helper extends Helper {
                   initialLocation = noLocation;
                   contentString = \"Error: Your browser doesn't support geolocation.\";
                 }
-                map.setCenter(initialLocation);
-                map.setZoom(3);
-            }";*/
+                gMap.setCenter(initialLocation);
+                gMap.setZoom(3);
+            }";	
 
-              /* $map .= "
+            $map .= "
             function setMarker(position){
                 var contentString = 'hello';
-                var image = '".$settings['icon']."';
+                var image = '".$settings['markerIcon']."';
                 var infowindow = new google.maps.InfoWindow({
                     content: contentString
                 });
                 var marker = new google.maps.Marker({
                     position: position,
-                    map: map,
+                    map: gMap,
                     title:\"My Position\"
                 });";
-            /*
-             if($infoWindow){
+            /*    
+             if($infoWindow){   
                  $map .= "google.maps.event.addListener(marker, 'click', function() {
                                 infowindow.open(map,marker);
                             });";
              }
              $map .= "}";*/
-
+            
             if($settings['usesJquery']){
-              $map .="
+              $map .=" 
               $(function(){
                 //initialize();
               });
@@ -228,63 +134,67 @@ class GoogleMapV3Helper extends Helper {
                 }
                 ";
               }
+              if($settings['localize']) $map .= "localize();"; 
+              
              $map .= "</script>";
         return $map;
     }
-
-
-    /**
-     * Function addMarker
-     *
+    
+    
+    /** 
+     * Function addMarker 
+     * 
      * This method puts a marker in the google map generated with the function map
-     *
+     * 
      * Pass an array with the options listed above in order to customize it
-     *
-     * @author Marc Fernandez <info (at) marcfg (dot) com>
-     * @param array $options - options array
+     * 
+     * @author Marc Fernandez <info (at) marcfg (dot) com> 
+     * @param array $options - options array 
      * @return string - will return all the javascript script to add the marker to the map
-     *
-     */
+     * 
+     */ 
     function addMarker($options){
         if($options==null) return null;
-        /*extract($options);
-        if(!isset($latitude) || $latitude==null || !isset($longitude) || $longitude==null) return null;
-        if (!preg_match("/[-+]?\b[0-9]*\.?[0-9]+\b/", $latitude) || !preg_match("/[-+]?\b[0-9]*\.?[0-9]+\b/", $longitude)) return null;
-        if(!isset($id)) $id=rand();
-        if(!isset($infoWindow)) $infoWindow=$this->defaultInfoWindowM;
-        if(!isset($windowText)) $windowText=$this->defaultWindowTextM;
-        */
-        $marker = "<script>";
-        //if(isset($markerIcon)) $marker .= "var image = '".$markerIcon."';";
-        //if(isset($shadowIcon)) $marker .= "var shadowImage = '".$shadowIcon."';";
-        $marker .= "
-
-
-          var myLatLng = new google.maps.LatLng(".$options['latitude'].", ".$options['longitude'].");
-                  var marker = new google.maps.Marker({
-                      position: myLatLng,
-                      map: map,";
-                    //if(isset($markerIcon)) $marker .= "icon: image,";
-                    //if(isset($shadowIcon)) $marker .= "shadow: shadowImage,";
-        $marker .= "
-            });";
+        
+        if(!isset($options['latitude']) || $options['latitude']==null || !isset($options['longitude']) || $options['longitude']==null) return null;
+        if (!preg_match("/[-+]?\b[0-9]*\.?[0-9]+\b/", $options['latitude']) || !preg_match("/[-+]?\b[0-9]*\.?[0-9]+\b/", $options['longitude'])) return null;        
+        
+        
+        $marker = "<script>";  
+          $marker .= "        
+            gMarkers.push(
+              new google.maps.Marker({
+               position:new google.maps.LatLng(".$options['latitude'].",".$options['longitude']."),
+               map:gMap,
+               icon:'".$options['icon']."'
+              }));
+        ";
             /*$marker .= "
            var contentString = '".$windowText."';
             var infowindow".$id." = new google.maps.InfoWindow({
                 content: contentString
             });";
-        if($infoWindow){
+        if($infoWindow){   
                  $marker .= "google.maps.event.addListener(marker".$id.", 'click', function() {
                                 infowindow".$id.".open(map,marker".$id.");
                             });";
         }*/
-        $marker .= "
-        map.
-        </script>";
+        $marker .= "</script>";
         return $marker;
     }
-
-
-}
-?>
-
+    
+    function autocenter()
+    {
+      $center="<script>";
+      $center.='
+      function auto_center(){
+        var bounds = new google.maps.LatLngBounds(); 
+        $.each(gMarkers,function (index, marker){ bounds.extend(marker.position);}); 
+        gMap.fitBounds(bounds);
+      }
+      auto_center();
+      </script>';
+      return $center;
+    }
+  }
+?> 
